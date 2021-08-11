af42a266ff24ac5c4f528480dc56f6c725b6db98
phesilva <phe.augustto@outlook.com>
2021-08-11 15:20:12 -0300

    Initial commit

---
+++ src/controller/OperacoesController.java
@@ -0,0 +1,56 @@
+package controller;
+
+public class OperacoesController {
+
+	public OperacoesController() {
+		super();
+	}
+	public void ConcatenaString () {
+		String cadeia = "";
+		//mili segundos
+		System.currentTimeMillis();
+		//nano segundos
+		double TempoInicial = System.nanoTime();
+		for(int i = 0; i < 32768;i++) {
+			cadeia = cadeia + "a";
+		}
+		double tempoFinal = System.nanoTime();
+		double tempoTotal = tempoFinal - TempoInicial;
+		
+		tempoTotal = tempoTotal / Math.pow(10, 9);
+		
+		System.out.println("String==>"+tempoTotal+"segundos");
+	}
+	public void concatenaBuffer() {
+		StringBuffer buffer = new StringBuffer();
+		double TempoInicial = System.nanoTime();
+		for(int i = 0; i < 32768;i++) {
+			buffer.append("a");
+		}
+		double tempoFinal = System.nanoTime();
+		double tempoTotal = tempoFinal - TempoInicial;
+		
+		tempoTotal = tempoTotal / Math.pow(10, 9);
+		
+		System.out.println("Buffer==>"+tempoTotal+"segundos");
+	}
+	public void divideFrase(String frase) {
+		String[] vetorPalavras = frase.split(" ");
+		
+	}
+	public void PercorrerVetor() {
+		String[] vetor = new String[100000];
+		double TempoInicial = System.nanoTime();
+		
+		for(int i = 0; i < 100000;i++) {
+			vetor[i] = "";
+		}
+		double tempoFinal = System.nanoTime();
+		double tempoTotal = tempoFinal - TempoInicial;
+		
+		//tempoTotal = tempoTotal / Math.pow(10, 9);
+		
+		System.out.println("vetor==>"+tempoTotal+"segundos");
+	}
+}
+//ctrl + d apaga a linha toda 
---
+++ src/view/Principal.java
@@ -0,0 +1,17 @@
+package view;
+
+import controller.OperacoesController;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		OperacoesController op = new OperacoesController();
+		op.ConcatenaString();
+		op.concatenaBuffer();
+		op.PercorrerVetor();
+		//48
+	}
+
+}
